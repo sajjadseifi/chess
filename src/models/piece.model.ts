5ea8cfed9ca5e8ab46a0698ecbfa45350d21b816
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-16 04:01:04 +0430

    build:piece(action interface)

--- src/models/piece.model.ts
+++ src/models/piece.model.ts
@@ -1,15 +1,28 @@
-export interface IPiece {
+import { Place } from '../types/index';
+
+export interface IPieceProps {
   id: string;
   name: string;
   row: number;
   col: number;
 }
+export interface IPieceAction {
+  freeSeats(): Place[][];
+  abilityToAttack(piece: IPieceProps): boolean;
+}
+export type IPiece = IPieceProps & IPieceProps;
 
-export class Piece implements IPiece {
+export class Piece implements IPieceProps, IPieceAction {
   row: number;
   col: number;
   constructor(public id: string, public name: string) {
     this.row = 0;
     this.col = 0;
+  }
+  freeSeats(): Place[][] {
+    throw new Error('Method not implemented.');
+  }
+  abilityToAttack(piece: IPieceProps): boolean {
+    throw new Error('Method not implemented.');
   }
 }
