1780b7901723d5ed87cd835095a97ec0f71a80b6
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-16 05:47:57 +0430

    fixed:game(add iface)

--- src/models/game.model.ts
+++ src/models/game.model.ts
@@ -1,12 +1,23 @@
 import { IPiece } from './index';
 
-export interface IGame {
+export interface IGameProperty {
+  player1: number;
+  player2: number;
+}
+
+export interface IGameOperational {
   attackers(piece: IPiece): IPiece[];
   beadsAttacked(piece: IPiece): IPiece[];
   isAttacked(piece: IPiece): boolean;
 }
 
-export class Game {
+export class Game implements IGameProperty, IGameOperational {
+  player1: number;
+  player2: number;
+  constructor() {
+    this.player1 = 1;
+    this.player2 = 2;
+  }
   beadsAttacked(piece: IPiece) {
     return [];
   }
